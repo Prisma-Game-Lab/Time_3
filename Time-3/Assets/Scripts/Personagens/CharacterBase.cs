be35a2cf9e0903bde310d26186309ba2af6dd1be
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-08-21 22:07:27 -0300

    Criação do Scriptable Object de personagens
    Esse Scriptable Object possui os status que imagino que serão compartilhados por todos os personagens do jogo. Pode ser usado para os protagonistas, inimigos e npcs.

---
+++ Time-3/Assets/Scripts/Personagens/CharacterBase.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Permite a criacao de instancias do SO pela janela "Project". -Arthur
+[CreateAssetMenu(fileName = "Novo personagem", menuName = "Personagem")]
+public class CharacterBase : ScriptableObject
+{
+    [Header("Atributos do jogador")]
+    public int baseHp;
+    public int baseDefense;
+    public int baseSpeed;
+
+    [Tooltip("Dano do ataque basico")]
+    public int baseDamage;
+
+    [Tooltip("Dano da habilidade de combate")]
+    public int baseSkillDmg;
+}
