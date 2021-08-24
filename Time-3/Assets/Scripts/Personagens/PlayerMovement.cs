7651b737f24aaabc6864ce1895c9d7a6b783d39b
LukaPedra <lucca.v.rocha@gmail.com>
2021-08-24 17:41:14 -0300

    Movimentação básica Hero

--- Time-3/Assets/Scripts/Personagens/PlayerMovement.cs
+++ Time-3/Assets/Scripts/Personagens/PlayerMovement.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public float Speed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,14 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //inputs
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+    }
+    private void FixedUpdate() 
+    {
+        //movimento
+        rb.MovePosition(rb.position + movement * Speed * Time.fixedDeltaTime);
     }
 }
