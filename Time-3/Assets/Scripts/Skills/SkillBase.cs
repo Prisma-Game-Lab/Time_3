78eecb1bd86af239bbe9372396502d749ee8f0f6
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-09-03 15:32:36 -0300

    Criação do Scriptable Object SkillBase
    Objeto que servirá como base para outras habilidades. Habilidades criadas no futuro devem herdar dessa classe

---
+++ Time-3/Assets/Scripts/Skills/SkillBase.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SkillBase : ScriptableObject
+{
+    public string skillName;
+    public float skillCD;
+    public float activeTime;
+
+    public string state = "ready"; 
+    public abstract void TriggerSkill(GameObject obj); 
+}
