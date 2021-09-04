4ecdb355b90659ac1e9b2ca5bcd828d697823010
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-09-04 11:03:58 -0300

    Criando duas habilidades dummy
    Elas servem para evitar o erro de nullreferenceexception. Quando tivermos habilidades de verdade, precisam ser apagadas.

---
+++ Time-3/Assets/Scripts/Skills/Scriptable Objects/Dummy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "dummy", menuName = "skills/dummy")]
+public class Dummy : SkillBase
+{
+    public int dummy = 0;
+    public int increase = 10;
+
+    public override void TriggerSkill(GameObject obj)
+    {
+       if(dummy == 0)
+       {
+           dummy += increase;
+           Debug.Log(dummy);
+       }
+       else
+       {
+           dummy -= increase;
+           Debug.Log(dummy);
+       }
+    }
+}
