aeedee269cb4817ce07f839db0cae0a61504dbcc
rafaelrc7 <rafaelrc7@gmail.com>
2021-09-05 18:26:35 -0300

    Rotacao inimiga suavizada

--- Time-3/Assets/Scripts/Personagens/Enemies/HostileMovementBehaviour.cs
+++ Time-3/Assets/Scripts/Personagens/Enemies/HostileMovementBehaviour.cs
@@ -2,29 +2,39 @@ using UnityEngine;
 
 public class HostileMovementBehaviour : MonoBehaviour
 {
-	[SerializeField] private float speed;
+	[SerializeField] private float rotSmooth = 5.0f;
 
 	private CharStats charStats;
 
-	private void Awake() => charStats = GetComponent<CharStats>();
-	private void Start() => speed = charStats.getSpeed();
+	private float speed;
+	private Quaternion targetDir;
+	private Vector3 targetPos;
 
-	public void MoveTo(Vector3 dest)
+	private void Awake() => charStats = GetComponent<CharStats>();
+
+	private void Start()
 	{
-		transform.position = Vector3.MoveTowards(transform.position, dest, speed * Time.deltaTime);
+		speed = charStats.getSpeed();
+		targetPos = transform.position;
+		targetDir = transform.rotation;
 	}
 
-	// TODO: Deixar rotacoes mais suaves
+	private void Update()
+	{
+		speed = charStats.getSpeed();
+		transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetDir, rotSmooth * Time.deltaTime);
+	}
+
+	public void MoveTo(Vector3 dest) => targetPos = dest;
+
+	public void Face(float angle) => targetDir = Quaternion.AngleAxis(angle, Vector3.forward);
+
 	public void Face(Vector3 target)
 	{
 		Vector2 lookDirection = target - transform.position;
 		float angle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
 		Face(angle);
-	}
-
-	public void Face(float angle)
-	{
-		transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
 	}
 
 	public void FaceRandom()
