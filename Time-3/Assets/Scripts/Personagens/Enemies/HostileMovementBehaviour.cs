0297134e22c67166ab2e80fcb57716ce7908c281
rafaelrc7 <rafaelrc7@gmail.com>
2021-09-24 17:48:44 -0300

    Implementando teste e uso do algoritimo A*

--- Time-3/Assets/Scripts/Personagens/Enemies/HostileMovementBehaviour.cs
+++ Time-3/Assets/Scripts/Personagens/Enemies/HostileMovementBehaviour.cs
@@ -1,19 +1,34 @@
+using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class HostileMovementBehaviour : MonoBehaviour
 {
 	[SerializeField] private float rotationSpeed = 5.0f;
 
 	private CharStats charStats;
+	private Rigidbody2D rb;
 
 	private float speed;
 	private Quaternion targetDir;
 	private Vector3 targetPos;
+	private Vector3 endTargetPos;
 
-	private void Awake() => charStats = GetComponent<CharStats>();
+	private Stack<Vector3> movementStack;
+	private Pathfinding pathfinding;
+
+	private void Awake()
+	{
+		charStats = GetComponent<CharStats>();
+		rb = GetComponent<Rigidbody2D>();
+		movementStack = new Stack<Vector3>();
+		endTargetPos = transform.position;
+		targetPos = transform.position;
+	}
 
 	private void Start()
 	{
+		pathfinding = Pathfinding.Instance;
 		speed = charStats.GetSpeed();
 		targetPos = transform.position;
 		targetDir = transform.rotation;
@@ -34,11 +49,28 @@ public class HostileMovementBehaviour : MonoBehaviour
 	public void UpdateMovement()
 	{
 		speed = charStats.GetSpeed();
-		transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-		transform.rotation = Quaternion.Lerp(transform.rotation, targetDir, rotationSpeed * Time.deltaTime);
+		rb.MovePosition(Vector2.MoveTowards(transform.position, targetPos, speed * Time.fixedDeltaTime));
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetDir, rotationSpeed * Time.fixedDeltaTime);
+
+		if (Mathf.Abs(Vector3.Distance(transform.position, targetPos)) < 0.1 && movementStack.Count > 0) {
+			targetPos = movementStack.Pop();
+		}
 	}
 
-	public void MoveTo(Vector3 dest) => targetPos = dest;
+	public void MoveTo(Vector3 dest)
+	{
+		if (Vector3.Distance(dest, endTargetPos) > 0.01) {
+			endTargetPos = dest;
+
+			movementStack = pathfinding.MakePath(transform.position, endTargetPos);
+			if (movementStack == null) {
+				movementStack = new Stack<Vector3>();
+				targetPos = transform.position;
+			} else {
+				targetPos = movementStack.Pop();
+			}
+		}
+	}
 
 	public void Face(float angle) => targetDir = Quaternion.AngleAxis(angle, Vector3.forward);
 
