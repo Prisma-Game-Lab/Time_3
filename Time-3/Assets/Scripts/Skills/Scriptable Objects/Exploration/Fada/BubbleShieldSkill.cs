f06cd043c46c4fa5ae6b1dbc429aa1e23c409a62
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-10-01 10:51:13 -0300

    Scriptable object da habilidade do escudo

---
+++ Time-3/Assets/Scripts/Skills/Scriptable Objects/Exploration/Fada/BubbleShieldSkill.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[CreateAssetMenu(fileName = "BubbleShield", menuName = "Exploration Skills/Bubble Shield")]
+public class BubbleShieldSkill : SkillBase
+{
+    [SerializeField] private GameObject shieldPrefab;
+
+    private GameObject player;
+
+    public override void TriggerSkill()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        Instantiate(shieldPrefab, player.transform.position + player.transform.right, player.transform.rotation);
+    }
+}
