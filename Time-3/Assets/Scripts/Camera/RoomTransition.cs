1448fb015d8a062b4b12ab2bb9d634864f0badc1
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-10-08 13:49:13 -0300

    Populando salas

--- Time-3/Assets/Scripts/Camera/RoomTransition.cs
+++ Time-3/Assets/Scripts/Camera/RoomTransition.cs
@@ -6,9 +6,27 @@ public class RoomTransition : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other) 
     {
+        PopulateRoom populateRoomScript = GetComponent<PopulateRoom>();
         if(other.CompareTag("Player"))
         {
+            if(CamController.instance.currentRoom.GetComponent<PopulateRoom>() != null)
+            {
+                CamController.instance.currentRoom.GetComponent<PopulateRoom>().Despawn();
+            }
+            
             CamController.instance.currentRoom = gameObject;
+
+            if(populateRoomScript != null)
+            {
+                if(populateRoomScript.GetPopulated() == false)
+                {
+                    populateRoomScript.Populate();
+                }
+                else
+                {
+                    populateRoomScript.Respawn();
+                }
+            }
         }   
     }
 }
---
+++ Time-3/Assets/Scripts/Level Generation/PopulateRoom.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopulateRoom : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> roomObjects = new List<GameObject>();
+    [SerializeField] private int totalObjects;
+
+    [SerializeField] private List<GameObject> spawnedObjects = new List<GameObject>();
+    private bool populated = false;
+
+    public void Populate()
+    {
+        for(int i = 0; i < totalObjects; i++)
+        {
+            float xPosition = Random.Range(-8,8);
+            float yPosition = Random.Range(-3,3);
+            int objectIndex = Random.Range(0, roomObjects.Count);
+
+            GameObject spawnedObject = Instantiate(roomObjects[objectIndex], new Vector3(xPosition,yPosition,0.0f) + gameObject.transform.position, Quaternion.identity);
+            spawnedObjects.Add(spawnedObject);
+        }
+        populated = true;
+    }
+
+    public void Despawn()
+    {
+        for(int i = 0; i < spawnedObjects.Count; i++)
+        {
+            spawnedObjects[i].SetActive(false);
+        }
+    }
+
+    public void Respawn()
+    {
+        for(int i = 0; i < spawnedObjects.Count; i++)
+        {
+            spawnedObjects[i].SetActive(true);
+        }
+    }
+
+    public bool GetPopulated()
+    {
+        return populated;
+    }
+}
