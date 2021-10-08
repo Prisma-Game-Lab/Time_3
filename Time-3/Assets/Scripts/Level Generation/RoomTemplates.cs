09c49a26041cb8a89ce9dd4806d5d0a85aa868a3
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-10-08 13:50:09 -0300

    Criando saída na fase

--- Time-3/Assets/Scripts/Level Generation/RoomTemplates.cs
+++ Time-3/Assets/Scripts/Level Generation/RoomTemplates.cs
@@ -11,14 +11,33 @@ public class RoomTemplates : MonoBehaviour
 
     public GameObject closedRoom;
 
+    public List<GameObject> rooms;
+
     public int maxRooms;
     public int minRooms;
 
     public int totalRooms;
     public int rand;
 
+    public float waitTime;
+    private bool exitSpawned;
+    public GameObject exit;
+
     private void Awake() 
     {
         rand = Random.Range(minRooms, maxRooms);
+    }
+
+    private void Update()
+    {   
+       if(waitTime <= 0 && exitSpawned == false)
+       {
+           Instantiate(exit, rooms[rooms.Count - 1].transform.position, Quaternion.identity);
+           exitSpawned = true;
+       } 
+       else
+       {
+           waitTime -= Time.deltaTime;
+       }
     }
 }
