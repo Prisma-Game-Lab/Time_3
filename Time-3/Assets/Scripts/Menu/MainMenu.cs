b85564582d43d258a9e2d0766a298bd7455e2c82
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2021-10-08 14:11:28 -0300

    Atualizando build setttings

--- Time-3/Assets/Scripts/Menu/MainMenu.cs
+++ Time-3/Assets/Scripts/Menu/MainMenu.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame(){
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1, LoadSceneMode.Single);
     }
     public void QuitGame(){
         Debug.Log("Quit");
