0126c27586c4accb180587470fe32febb62c9aa4
rafaelrc7 <rafaelrc7@gmail.com>
2021-10-08 22:55:05 -0300

    Bugfix: inimigos ignorando parede de bolhas

--- Time-3/Assets/Scripts/Personagens/Enemies/HostileMovementBehaviour.cs
+++ Time-3/Assets/Scripts/Personagens/Enemies/HostileMovementBehaviour.cs
@@ -5,12 +5,17 @@ public class HostileMovementBehaviour : MonoBehaviour
 	[SerializeField] private float rotationSpeed = 5.0f;
 
 	private CharStats charStats;
+	private Rigidbody2D rb2D;
 
 	private float speed;
 	private Quaternion targetDir;
 	private Vector3 targetPos;
 
-	private void Awake() => charStats = GetComponent<CharStats>();
+	private void Awake()
+	{
+		charStats = GetComponent<CharStats>();
+		rb2D = GetComponent<Rigidbody2D>();
+	}
 
 	private void Start()
 	{
@@ -34,8 +39,8 @@ public class HostileMovementBehaviour : MonoBehaviour
 	public void UpdateMovement()
 	{
 		speed = charStats.GetSpeed();
-		transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
-		transform.rotation = Quaternion.Lerp(transform.rotation, targetDir, rotationSpeed * Time.deltaTime);
+		rb2D.MovePosition(Vector2.MoveTowards(transform.position, targetPos, speed * Time.fixedDeltaTime));
+		transform.rotation = Quaternion.Lerp(transform.rotation, targetDir, rotationSpeed * Time.fixedDeltaTime);
 	}
 
 	public void MoveTo(Vector3 dest) => targetPos = dest;
