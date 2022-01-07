b6ea486c6bda9fbcba60174193a50e0d2391be2e
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-01-07 16:52:23 -0300

    Tela de loading

--- Time-3/Assets/Scripts/Menu/StartLevel.cs
+++ Time-3/Assets/Scripts/Menu/StartLevel.cs
@@ -1,15 +1,32 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StartLevel : MonoBehaviour
 {
+    [SerializeField] private string levelToGo;
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Slider slider;
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Player"))
         {
-            SceneManager.LoadScene("Level", LoadSceneMode.Single);
+            StartCoroutine(LoadLevelAsync());
+        }
+    }
+
+    private IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(levelToGo);
+
+        loadingScreen.SetActive(true);
+
+        while(!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / 0.9f);
+            slider.value = progress;
+            yield return null;
         }
     }
 }
