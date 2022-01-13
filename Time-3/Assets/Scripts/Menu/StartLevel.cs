4b5b24cbb1191ed09e3f1175201d34beb1931301
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-01-13 18:13:27 -0300

    Tela de ir para proxima fase

--- Time-3/Assets/Scripts/Menu/StartLevel.cs
+++ Time-3/Assets/Scripts/Menu/StartLevel.cs
@@ -7,19 +7,27 @@ public class StartLevel : MonoBehaviour
 {
     [SerializeField] private string levelToGo;
     [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private GameObject levelPopup;
     [SerializeField] private Slider slider;
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.CompareTag("Player"))
         {
-            StartCoroutine(LoadLevelAsync());
+            levelPopup.SetActive(true);
+            Time.timeScale = 0.0f;
         }
+    }
+
+    public void StartLvl()
+    {
+        StartCoroutine(LoadLevelAsync());
     }
 
     private IEnumerator LoadLevelAsync()
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(levelToGo);
 
+        levelPopup.SetActive(true);
         loadingScreen.SetActive(true);
 
         while(!operation.isDone)
