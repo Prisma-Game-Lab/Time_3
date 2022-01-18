0b99358adf5bd4daba7b7abf76cd11bee5190a0f
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-01-18 18:59:52 -0300

    Frases aleatórias dos NPCs

--- Time-3/Assets/Scripts/Personagens/NPC/NPCbase.cs
+++ Time-3/Assets/Scripts/Personagens/NPC/NPCbase.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class NPCbase : MonoBehaviour
 {
-    public Dialogue dialogue;
+    public Dialogue[] dialogues;
+    private Dialogue dialogue;
+
+    private void Start() 
+    {
+        int dialogueIndex = Random.Range(0, dialogues.Length);
+        dialogue = dialogues[dialogueIndex];
+    }
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
