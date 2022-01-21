d0d325db079d503a7633c033d58d2aa1842daecc
AlOzorio <39777406+AlOzorio@users.noreply.github.com>
2022-01-21 19:38:48 -0300

    Pirata não pode mais usar bombas infinitas

--- Time-3/Assets/Scripts/Skills/Scriptable Objects/Exploration/Pirata/PirateMineBehaviour.cs
+++ Time-3/Assets/Scripts/Skills/Scriptable Objects/Exploration/Pirata/PirateMineBehaviour.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class PirateMineBehaviour : MonoBehaviour
 {
 	[SerializeField] private float detect_radius;
 	[SerializeField] private float blast_radius;
 	[SerializeField] private int blast_damage;
+	[SerializeField] private int blast_time;
 
+	private void Awake() 
+	{
+		StartCoroutine(StartBlast());
+	}
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		Collider2D[] damageables = Physics2D.OverlapCircleAll(transform.position, blast_radius);
@@ -26,5 +33,24 @@ public class PirateMineBehaviour : MonoBehaviour
 		Destroy(gameObject);
 	}
 
+
+	private IEnumerator StartBlast()
+	{
+		yield return new WaitForSeconds(blast_time);
+
+		Collider2D[] damageables = Physics2D.OverlapCircleAll(transform.position, blast_radius);
+
+		FindObjectOfType<AudioManager>().Play("pirata ovo explode");
+		
+		foreach (var entity in damageables) {
+			if (entity.tag == "Player" || entity.tag == "Ignore") continue;
+			IDamageable<int> damageable = entity.GetComponent<IDamageable<int>>();
+			if (damageable != null) {
+				damageable.ApplyDamage(blast_damage);
+			}
+		}
+
+		Destroy(gameObject);
+	}
 }
 
